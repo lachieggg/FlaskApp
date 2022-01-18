ca932965055ebbb9c73b1494711214d355b7449f
Lachlan Grant <grant.lachlan.j@gmail.com>
2022-01-19 06:27:41 +1100

    Added template

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask import render_template
+
+from os import getcwd
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+	return "<p>Hello, World!</p>"
+
+@app.route("/index")
+def index(name=None):
+	return render_template("index.html", name=name)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
