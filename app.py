7b1defac285ff8b6b5734d7806821c6bb44859b1
Lachlan Grant <grant.lachlan.j@gmail.com>
2022-01-19 07:57:09 +1100

    Dockerized application

--- app.py
+++ app.py
@@ -3,9 +3,17 @@
 from flask import Flask
 from flask import render_template
 
-from os import getcwd
+import os
+import ast
 
 app = Flask(__name__)
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FLASK_PUBLIC = ast.literal_eval(os.getenv('FLASK_PUBLIC'))
+PUBLIC_HOST = "0.0.0.0"
 
 @app.route("/")
 def hello_world():
@@ -16,4 +24,5 @@ def index(name=None):
 	return render_template("index.html", name=name)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	host = PUBLIC_HOST if FLASK_PUBLIC else None
+	app.run(debug=True, host=host)
